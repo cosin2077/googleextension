86900711c44b842e1871a43823678c9c54f24c68
conanskyforce <conanskyforce@gmail.com>
2016-11-02 15:55:31 +0800

    up

--- my_clock/my_clock.js
+++ my_clock/my_clock.js
@@ -1,11 +1,15 @@
 function my_clock(el){
     var today=new Date();
+    var year=today.getFullYear();
+    var month=today.getMonth();
+    var day=today.getDate();
+    var d=today.getDay();
     var h=today.getHours();
     var m=today.getMinutes();
     var s=today.getSeconds();
     m=m>=10?m:('0'+m);
     s=s>=10?s:('0'+s);
-    el.innerHTML = h+":"+m+":"+s;
+    el.innerHTML ="今天是"+"："+"<br/>"+year+"年"+"<br/>"+month+"月"+day+"日"+"<br/>"+"星期"+day+"<br/>"+ h+":"+m+":"+s;
     setTimeout(function(){my_clock(el)}, 1000);
 }
 
