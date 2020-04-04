731d62ea0fd329af3fc355dbcdf17420466c37e9
conanskyforce <conanskyforce@163.com>
2020-04-04 18:06:43 +0800

    add clock

---
+++ block_sponsor/my_clock.js
@@ -0,0 +1,17 @@
+function my_clock(el){
+    var today=new Date();
+    var year=today.getFullYear();
+    var month=today.getMonth();
+    var day=today.getDate();
+    var d=today.getDay();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m=m>=10?m:('0'+m);
+    s=s>=10?s:('0'+s);
+    el.innerHTML =h+":"+m+":"+s;
+    setTimeout(function(){my_clock(el)}, 1000);
+}
+
+var clock_div = document.getElementById('clock_div');
+my_clock(clock_div);
